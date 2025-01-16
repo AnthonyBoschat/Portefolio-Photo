070992072e4c23a297933be10e3fd4542fd11f24
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-01-16 22:58:36 +0100

    Initialisation projet frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "@Pages/Home";
+
+
+export default function App() {
+  
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
---
+++ Frontend/src/Pages/Home/index.jsx
@@ -0,0 +1,13 @@
+// import "./style.scss";
+
+export default function HomePage(){
+
+
+
+    return(
+        <>
+            <h1>HomePage</h1>
+            
+        </>
+    )
+}
